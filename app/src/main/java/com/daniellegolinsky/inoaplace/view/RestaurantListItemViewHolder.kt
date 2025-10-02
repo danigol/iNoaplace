6d1382792739e41f75f3d3360c3a98fb4f615803
Danielle Golinsky <degolinsky@icloud.com>
2025-10-02 18:12:11 -0400

    Some fixes.

--- app/src/main/java/com/daniellegolinsky/inoaplace/view/RestaurantListItemViewHolder.kt
+++ app/src/main/java/com/daniellegolinsky/inoaplace/view/RestaurantListItemViewHolder.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.daniellegolinsky.inoaplace.R
 import com.daniellegolinsky.inoaplace.model.RestaurantInfo
 
-class RestaurantListItemViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+class RestaurantListItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     private var itemName: TextView = view.findViewById(R.id.item_name)
     private var itemHealthScore: TextView = view.findViewById(R.id.item_health_score)
     private var itemBorough: TextView = view.findViewById(R.id.item_borough)
